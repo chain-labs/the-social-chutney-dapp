36e7c330c02b3293cbc5156cdcc4022f1aecb74e
Angel Lakra <angel.lakra@chainlabs.in>
2023-03-25 06:52:12 +0530

    added configuration

--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -5,21 +5,21 @@ export const BUTTON_TEXT_COLOR = BACKGROUND_COLOR;
 export const TOKEN_COUNTER_COLOR = "#db3e77";
 export const INPUT_BORDER_COLOR = TEXT_COLOR;
 
-export const COLLECTION_NAME = "TSC Digital Merchandise";
+export const COLLECTION_NAME = "Rao Jodha: Founder of Jodhpur";
 
 export const COLLECTION_DESCRIPTION = [
-  "The Social Chutney 3.0: A Unique Blend of Tradition and Technology. Claim to participate in Lucky Draw.",
+  "Rao Jodha, of the Rathore Clan. The founder of Jodhpur. Generated using AI.",
 ];
 
-export const MINT_BUTTON_TEXT = "Claim Merch";
+export const MINT_BUTTON_TEXT = "Claim Piece";
 
-export const TOKEN_NAME = "Merchandise";
+export const TOKEN_NAME = "Historic Piece";
 
 export const SHOW_TOKENS_CLAIMED = true;
 export const SHOW_TOTAL_TOKENS = false;
 
 export const HERO_MEDIA =
-  "https://ik.imagekit.io/xlvg9oc4k/jodhpur_main_1_tsc.png?updatedAt=1679688251434";
+  "https://ik.imagekit.io/xlvg9oc4k/jodhpur_main_2_tsc.png?updatedAt=1679688227322";
 export const LOGO_MEDIA =
   "https://ik.imagekit.io/chainlabs/Mint_Pages/The_Social_Chutney/Tsc_logo_2_WPyFBNwug.png?updatedAt=1679597931525";
 export const LOGO_MEDIA_2 =
@@ -30,7 +30,7 @@ export const FAVICON_URL =
 
 export const TOKEN_IMG_URI = process.env.NEXT_PUBLIC_TOKEN_IMAGE_URL;
 
-export const WEBSITE_TITLE = "The Social Chutney | Claim your Merch";
+export const WEBSITE_TITLE = "The Social Chutney | Treasure Hunt";
 
 export const TWITTER_URL = "";
 export const DISCORD_URL = "";
